576958a548220e108628d5aed1138adca0a09fa6
Aleksandar Krastev <alexalex@mit.edu>
2022-09-07 13:58:20 -0400

    Setup .bashrc.d

---
+++ cli/.bashrc.d/shared.sh
@@ -0,0 +1,16 @@
+[ -r /usr/share/bash-completion/bash_completion ] && . /usr/share/bash-completion/bash_completion
+
+alias cp="cp -i"                          # confirm before overwriting something
+alias rm="rm -i"
+alias df='df -h'                          # human-readable sizes
+alias free='free -m'                      # show sizes in MB
+alias mosh='mosh --experimental-remote-ip=remote'
+
+
+set -o vi
+export EDITOR=vim
+
+
+if [ -n "$VIRTUAL_ENV" ]; then
+    source "$VIRTUAL_ENV/bin/activate"
+fi
---
+++ laptop/.bashrc.d/laptop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install Ruby Gems to ~/gems
+export GEM_HOME="$(ruby -e 'puts Gem.user_dir')"
+export PATH="$PATH:$GEM_HOME/bin"
+
+export CUPS_USER=alexalex
+export XDG_DATA_HOME=$HOME/.local/share
+
+alias hdmi-present="xrandr --output HDMI-2 --auto --right-of eDP-1"
+
+# better yaourt colors
+export YAOURT_COLORS="nb=1:pkg=1:ver=1;32:lver=1;45:installed=1;42:grp=1;34:od=1;41;5:votes=1;44:dsc=0:other=1;35"
+
+alias np='nano -w PKGBUILD'
+
+xhost +local:root > /dev/null 2>&1
