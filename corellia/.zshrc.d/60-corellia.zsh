66de8bbd6f2d82cc5e7c78c0e16a8b6851a242a5
Aleksandar Krastev <alexalex@mit.edu>
2024-07-15 10:38:50 -0400

    [corellia] minispec paths

--- corellia/.zshrc.d/60-corellia.zsh
+++ corellia/.zshrc.d/60-corellia.zsh
@@ -11,7 +11,8 @@ source /afs/csail.mit.edu/system/common/etc/zsh/zprofile.csail
 
 #export PATH="/data/sanchez/tools/gcc-8.2/bin:/data/sanchez/tools/gcc-8.2/bin:/usr/local/csail/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/afs/athena.mit.edu/course/6/6.004/yosys/bin:/snap/bin:/data/sanchez/tools/qmn/18.04/bsc-vagrant/bin:/data/sanchez/tools/nsamar/18.04/minispec/synth:/afs/athena.mit.edu/course/6/6.004/yosys/bin:/data/sanchez/tools/nsamar/18.04/minispec:/data/sanchez/tools/qmn/18.04/bsc-vagrant/bin:/data/sanchez/tools/nsamar/18.04:/data/sanchez/tools/qmn/18.04/bsc-vagrant/bin:/data/sanchez/tools/nsamar/18.04/minispec/synth:/afs/athena.mit.edu/course/6/6.004/yosys/bin:/data/sanchez/tools/nsamar/18.04/minispec:$PATH"
 #source /data/sanchez/tools/gcc-8.2/paths.sh
-#source /data/sanchez/tools/nsamar/18.04/paths-vagrant.sh
+#source /data/sanchez/tools/qmn/22.04/minispec/paths-vagrant.sh
+source /data/sanchez/tools/yifany/minispec/paths.sh
 
 export PATH="/afs/csail.mit.edu/u/a/alexalex/.local/bin:$PATH"
 export GOPATH="/data/scratch/alexalex/.go"
