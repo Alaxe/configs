3965a71c31129895243f96d0357a18f712b5d95d
Aleksandar Krastev <alexalex@mit.edu>
2026-08-18 15:29:11 -0400

    [zsh] Prompt for parallel clones of a repo

--- cli/.zshrc.d/20.base.zsh
+++ cli/.zshrc.d/20.base.zsh
@@ -21,12 +21,29 @@ if [ -n "$VIRTUAL_ENV" ]; then
     source "$VIRTUAL_ENV/bin/activate"
 fi
 
-PS1='%B%F{10}[%m%F{white} %2~%F{10}]$%f%b '
-# for root
-#PS1='%B%F{red}[%m%F{14} %2~%F{red}]#%f%b '
-
 export PATH="$HOME/.local/bin:$PATH"
 export SCONSFLAGS="--random $SCONSFLAGS"
 
-precmd () { print -Pn "\e]0;[%M %2~]\a" }
-preexec () { print -Pn "\e]0;[%M %2~] $1\a" }
+setopt prompt_subst
+
+CLONE_SLOTS=(alderaan bespin coruscant dagobah)
+
+clone_tag() {
+  local p
+  CLONE_TAG=''
+  unset SLOT
+  for p in ${(s:/:)PWD}; do
+    if (( ${CLONE_SLOTS[(Ie)${p##*-}]} )); then
+      export SLOT=${p##*-}
+      CLONE_TAG="$SLOT "
+      return
+    fi
+  done
+}
+chpwd_functions+=(clone_tag)
+clone_tag
+
+precmd()  { print -Pn "\e]0;[%M %2~]\a" }
+preexec() { print -Pn "\e]0;[%M %2~] ${1//\%/%%}\a" }
+
+PS1='%B%F{10}[%m %F{13}${CLONE_TAG}%F{white}%2~%F{10}]$%f%b '
