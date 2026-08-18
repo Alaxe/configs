d9ee4aed5fde8d1271e6e86db145ecf9dafda9c9
Aleksandar Krastev <alexalex@mit.edu>
2026-08-18 15:28:03 -0400

    [zsh] minor clean up

--- corellia/.zshrc.d/60-corellia.zsh
+++ corellia/.zshrc.d/60-corellia.zsh
@@ -6,8 +6,4 @@ tmux() {
     krenew -t -- tmux $@
 }
 
-#export PATH="/afs/csail.mit.edu/u/a/alexalex/.local/bin:$PATH"
-#export GOPATH="/data/scratch/alexalex/.go"
-#export GOCACHE="/data/scratch/alexalex/.cache/go"
-
 export SCONSFLAGS="-j32 $SCONSFLAGS"
